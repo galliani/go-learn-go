32199ea6b1bc5233cebb1a092af37a9657a1a06b
Galih Muhammad <galih0muhammad@gmail.com>
2017-11-02 21:59:36 +0700

    Try separating package into multiple files

---
+++ fund.go
@@ -0,0 +1,26 @@
+package main
+
+type Fund struct {
+    // balance is unexported (private), because it's lowercase
+    // So what we are doing here is similiar to declaring attributes that belong to Fund type
+    balance int
+}
+
+// A regular function returning a pointer to a fund
+func NewFund(initialBalance int) *Fund {
+    // We can return a pointer to a new struct without worrying about
+    // whether it's on the stack or heap: Go figures that out for us.
+    return &Fund{
+        balance: initialBalance,
+    }
+}
+
+// Methods start with a *receiver*, in this case a Fund pointer
+func (f *Fund) Balance() int {
+    // Remember that f is an instance of Fund type Struct that does have balance attribute
+    return f.balance
+}
+
+func (f *Fund) Withdraw(amount int) {
+    f.balance -= amount
+}
--- main.go
+++ main.go
@@ -2,32 +2,14 @@ package main
 
 import "fmt"
 
-type Fund struct {
-    // balance is unexported (private), because it's lowercase
-    // So what we are doing here is similiar to declaring attributes that belong to Fund type
-    balance int
-}
-
-// A regular function returning a pointer to a fund
-func NewFund(initialBalance int) *Fund {
-    // We can return a pointer to a new struct without worrying about
-    // whether it's on the stack or heap: Go figures that out for us.
-    return &Fund{
-        balance: initialBalance,
-    }
-}
-
-// Methods start with a *receiver*, in this case a Fund pointer
-func (f *Fund) Balance() int {
-    // Remember that f is an instance of Fund type Struct that does have balance attribute
-    return f.balance
-}
-
-func (f *Fund) Withdraw(amount int) {
-    f.balance -= amount
-}
-
-
+// Since we put all the Fund related functions in a separate file, 
+// but still in same package "main", to validly run this script we have to type either:
+// go run *.go
+//      or
+// go build
+// then -> ./hello_go
+// 
+// After that, we should clean the exe bin by typing "go clean" in terminal
 func main() {
     fund := NewFund(100)
     fmt.Printf("%v \n", fund)
@@ -43,32 +25,33 @@ func main() {
         fmt.Println("Damn, you have got no money left!")
     }
 
-    // Shorthand syntax of declaring and initializing vars
-    // a := 10
-    // b := "golang"
-    // c := 4.17
-    // d := true
-
-    // // Longform syntax
-    // // Declaration
-    // var a int
-    // var b string
-    // var c float64
-    // var d bool
-    // // Initializing
-    // a = 10
-    // b = "golang"
-    // c = 4.17
-    // d = true
-
-    // fmt.Printf("%v \n", a)
-    // fmt.Printf("%v \n", b)
-    // fmt.Printf("%v \n", c)
-    // fmt.Printf("%v \n", d)    
-    
-    // fmt.Printf("%T \n", a)
-    // fmt.Printf("%T \n", b)
-    // fmt.Printf("%T \n", c)
-    // fmt.Printf("%T \n", d)
-
 }
+
+// Just some notes about variable in go:
+// Shorthand syntax of declaring and initializing vars
+// a := 10
+// b := "golang"
+// c := 4.17
+// d := true
+
+// // Longform syntax
+// // Declaration
+// var a int
+// var b string
+// var c float64
+// var d bool
+// // Initializing
+// a = 10
+// b = "golang"
+// c = 4.17
+// d = true
+
+// fmt.Printf("%v \n", a)
+// fmt.Printf("%v \n", b)
+// fmt.Printf("%v \n", c)
+// fmt.Printf("%v \n", d)    
+
+// fmt.Printf("%T \n", a)
+// fmt.Printf("%T \n", b)
+// fmt.Printf("%T \n", c)
+// fmt.Printf("%T \n", d)
